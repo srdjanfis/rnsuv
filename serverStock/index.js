5c9a27496e959fe527f67fbcac83b367741b3873
srdjanfis <130354228+srdjanfis@users.noreply.github.com>
2024-12-21 10:21:58 +0100

    Add files via upload

---
+++ serverStock/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors=require('cors');
+const app = express()
+
+app.use(bodyParser());
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get('/api/fail', (req, res) => res.status(403).json({msg: 'You are not allowed to access this'}));
+
+app.use('/api/stock', require('./stocks'));
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
