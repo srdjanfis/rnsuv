75f16ba8f5061a70779cb24c7778f34fd98793d8
srdjanfis <130354228+srdjanfis@users.noreply.github.com>
2025-01-09 13:51:23 +0100

    Add files via upload

---
+++ studentski-referat-server/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+var jwt = require('jsonwebtoken');
+const app = express()
+
+app.use(bodyParser());
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get('/api/fail', (req, res) => res.status(403).json({msg: 'You are not allowed to access this'}));
+
+app.use('/api/student', function(req, res, next) {
+    var token = req.get('Authorization');
+    if(token){
+      console.log(token.substring(7));
+      var user = jwt.decode(token.substring(7));
+      console.log(user);
+      if (user && user.user) {
+        return next();
+      }
+    }
+    return res.status(403).json({msg: 'Please login to access this information'});
+  }, require('./students'));
+app.use('/api/user', require('./user'));
+
+app.listen(3000, () => console.log('Student app backend listening on port 3000!'));
